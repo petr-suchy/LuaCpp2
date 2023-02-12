6aaaadc9e2246ef31c32d6c670abe5c7c61c9d71
Petr Suchý <petrs.sokolo@seznam.cz>
2023-02-12 11:29:50 +0100

    adds Function test cases

---
+++ tests/src/State/FunctionTests.cpp
@@ -0,0 +1,69 @@
+#include <boost/test/unit_test.hpp>
+#include "../VsTestExplorer.h"
+
+#include "../../../include/Function.h"
+
+BOOST_AUTO_TEST_SUITE(State__Function)
+
+BOOST_AUTO_TEST_CASE(shouldShareMutableValue)
+{
+	Lua::State state;
+
+	state.open();
+	BOOST_TEST(state.isOpen());
+
+	int counter = 1;
+	int result = 0;
+	
+	Lua::Function f(
+		[counter, &result] (Lua::State&) mutable
+		{
+			result = counter++;
+			return 0;
+		}
+	);
+
+	state << f;
+	state.pcall();
+	BOOST_TEST(result == 1);
+
+	Lua::Function f2;
+	state << f;
+	state >> f2;
+
+	state << f2;
+	state.pcall();
+	BOOST_TEST(result == 2);
+
+	state << f;
+	state.pcall();
+	BOOST_TEST(result == 3);
+
+	state << f2;
+	state.pcall();
+	BOOST_TEST(result == 4);
+	
+}
+
+BOOST_AUTO_TEST_CASE(shouldHaveRawPointer)
+{
+	Lua::State state;
+
+	state.open();
+	BOOST_TEST(state.isOpen());
+
+	Lua::Function f(
+		[] (Lua::State&) mutable
+		{
+			return 0;
+		}
+	);
+
+	Lua::Function f2;
+	state << f;
+	state >> f2;
+
+	BOOST_TEST(state.getL() == f2.getL());
+}
+
+BOOST_AUTO_TEST_SUITE_END()
