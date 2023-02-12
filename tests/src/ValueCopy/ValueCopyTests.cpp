05da201434707e7180d5b32e21c6756223b3cd7a
Petr Suchý <petrs.sokolo@seznam.cz>
2023-02-12 11:32:44 +0100

    adds ValueCopy test cases

---
+++ tests/src/ValueCopy/ValueCopyTests.cpp
@@ -0,0 +1,36 @@
+#include <boost/test/unit_test.hpp>
+#include "../VsTestExplorer.h"
+
+#include "../../../include/Reference.h"
+#include "../../../include/ReadableInteger.h"
+#include "../../../include/WritableInteger.h"
+#include "../../../include/ValueCopy.h"
+
+BOOST_AUTO_TEST_SUITE(ValueCopy__ValueCopy)
+
+BOOST_AUTO_TEST_CASE(testInsertionAndRetrieval)
+{
+	Lua::State state;
+
+	state.open();
+	BOOST_TEST(state.isOpen());
+
+	state << Lua::WritableInteger<int>(123);
+
+	Lua::ValueCopy copy;
+	state >> copy;
+
+	BOOST_TEST(state.getStackTop() == 0);
+
+	state << copy;
+
+	BOOST_TEST(state.getStackTop() == 1);
+
+	int num;
+	Lua::ReadableInteger<int> rn(num);
+	state >> rn;
+
+	BOOST_TEST(num == 123);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
