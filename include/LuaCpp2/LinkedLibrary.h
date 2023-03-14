61406b67515f0547ad5e5cd335d79ef268b92c11
Petr Suchý <petrs.sokolo@seznam.cz>
2023-03-14 19:46:28 +0100

    Added library implementation.

---
+++ include/LuaCpp2/LinkedLibrary.h
@@ -0,0 +1,223 @@
+#pragma once
+
+#include "Library.h"
+#include "lua.hpp"
+
+namespace Lua {
+
+    template<typename NewState>
+    class LinkedLibrary : public Library {
+    public:
+
+        /* State manipulation */
+
+        virtual State* newstate()
+        {
+            return NewState{}();
+        }
+
+        virtual void close(State* L)
+        {
+            lua_close(L);
+        }
+
+        /* Stack */
+
+        virtual int gettop(State* L)
+        {
+            return lua_gettop(L);
+        }
+
+        virtual void settop(State* L, int idx)
+        {
+            lua_settop(L, idx);
+        }
+
+        virtual int checkstack(State* L, int n)
+        {
+            return lua_checkstack(L, n);
+        }
+
+		/* Manipulation of elements */
+
+		virtual void pop(State* L, int n)
+		{
+			lua_pop(L, n);
+		}
+
+		virtual void pushvalue(State* L, int idx)
+		{
+			lua_pushvalue(L, idx);
+		}
+
+		virtual void remove(State* L, int idx)
+		{
+			lua_remove(L, idx);
+		}
+
+		virtual void insert(State* L, int idx)
+		{
+			lua_insert(L, idx);
+		}
+
+		virtual void replace(State* L, int idx)
+		{
+			lua_replace(L, idx);
+		}
+
+		/* Pushing elements */
+
+		virtual void pushboolean(State* L, int b)
+		{
+			lua_pushboolean(L, b);
+		}
+
+		virtual void pushinteger(State* L, Integer n)
+		{
+			lua_pushinteger(L, n);
+		}
+
+		virtual void pushnil(State* L)
+		{
+			lua_pushnil(L);
+		}
+
+		virtual void pushnumber(State* L, Number n)
+		{
+			lua_pushnumber(L, n);
+		}
+
+		virtual void pushstring(State* L, const char* s)
+		{
+			lua_pushstring(L, s);
+		}
+
+		virtual void pushlstring(State* L, const char* s, size_t len)
+		{
+			lua_pushlstring(L, s, len);
+		}
+
+		/* Testing elements */
+
+		virtual int type(State* L, int idx)
+		{
+			return lua_type(L, idx);
+		}
+
+		virtual int isboolean(State* L, int idx)
+		{
+			return lua_isboolean(L, idx);
+		}
+
+		virtual int iscfunction(State* L, int idx)
+		{
+			return lua_iscfunction(L, idx);
+		}
+
+		virtual int isfunction(State* L, int idx)
+		{
+			return lua_isfunction(L, idx);
+		}
+
+		virtual int islightuserdata(State* L, int idx)
+		{
+			return lua_islightuserdata(L, idx);
+		}
+
+		virtual int isnil(State* L, int idx)
+		{
+			return lua_isnil(L, idx);
+		}
+
+		virtual int isnumber(State* L, int idx)
+		{
+			return lua_isnumber(L, idx);
+		}
+
+		virtual int isstring(State* L, int idx)
+		{
+			return lua_isstring(L, idx);
+		}
+
+		virtual int istable(State* L, int idx)
+		{
+			return lua_istable(L, idx);
+		}
+
+		virtual int isthread(State* L, int idx)
+		{
+			return lua_isthread(L, idx);
+		}
+
+		virtual int isuserdata(State* L, int idx)
+		{
+			return lua_isuserdata(L, idx);
+		}
+
+		/* Querying elements */
+
+		virtual int toboolean(State* L, int idx)
+		{
+			return lua_toboolean(L, idx);
+		}
+
+		virtual Integer tointeger(State* L, int idx)
+		{
+			return lua_tointeger(L, idx);
+		}
+
+		virtual Number tonumber(State* L, int idx)
+		{
+			return lua_tonumber(L, idx);
+		}
+
+		virtual const char* tostring(State* L, int idx)
+		{
+			return lua_tostring(L, idx);
+		}
+
+		virtual const char* tolstring(State* L, int idx, size_t* len)
+		{
+			return lua_tolstring(L, idx, len);
+		}
+
+		/* Tables */
+
+		virtual void createtable(State* L, int narr, int nrec)
+		{
+			lua_createtable(L, narr, nrec);
+		}
+
+		virtual void settable(State* L, int idx)
+		{
+			lua_settable(L, idx);
+		}
+
+		virtual void gettable(State* L, int idx)
+		{
+			lua_gettable(L, idx);
+		}
+
+		virtual void setfield(State* L, int idx, const char* k)
+		{
+			lua_setfield(L, idx, k);
+		}
+
+		virtual void getfield(State* L, int idx, const char* k)
+		{
+			lua_getfield(L, idx, k);
+		}
+
+		virtual void setmetatable(State* L, int idx)
+		{
+			lua_setmetatable(L, idx);
+		}
+
+		virtual int getmetatable(State* L, int idx)
+		{
+			return lua_getmetatable(L, idx);
+		}
+
+    };
+
+}
---
+++ tests/src/Library/LibraryTests.cpp
@@ -0,0 +1,255 @@
+#include <boost/test/unit_test.hpp>
+#include "../VsTestExplorer.h"
+
+#include <LuaCpp2/Library.h>
+
+BOOST_AUTO_TEST_SUITE(Library__Library)
+
+BOOST_AUTO_TEST_CASE(testNewState)
+{
+	Lua::Library::State* L = Lua::Library::inst().newstate();
+	BOOST_TEST(L != nullptr);
+	Lua::Library::inst().close(L);
+}
+
+BOOST_AUTO_TEST_CASE(testStackTop)
+{
+	Lua::Library::State* L = Lua::Library::inst().newstate();
+
+	BOOST_TEST(Lua::Library::inst().gettop(L) == 0);
+	BOOST_TEST(Lua::Library::inst().checkstack(L, 3));
+
+	Lua::Library::inst().pushinteger(L, 10);
+	BOOST_TEST(Lua::Library::inst().gettop(L) == 1);
+
+	Lua::Library::inst().pushinteger(L, 20);
+	BOOST_TEST(Lua::Library::inst().gettop(L) == 2);
+
+	Lua::Library::inst().pushinteger(L, 30);
+	BOOST_TEST(Lua::Library::inst().gettop(L) == 3);
+
+	Lua::Library::inst().settop(L, 0);
+	BOOST_TEST(Lua::Library::inst().gettop(L) == 0);
+
+	Lua::Library::inst().settop(L, 3);
+	BOOST_TEST(Lua::Library::inst().gettop(L) == 3);
+
+	BOOST_TEST(Lua::Library::inst().isnil(L, 1));
+	BOOST_TEST(Lua::Library::inst().isnil(L, 2));
+	BOOST_TEST(Lua::Library::inst().isnil(L, 3));
+
+	Lua::Library::inst().pop(L, 2);
+	BOOST_TEST(Lua::Library::inst().gettop(L) == 1);
+
+	Lua::Library::inst().pop(L, 1);
+	BOOST_TEST(Lua::Library::inst().gettop(L) == 0);
+
+	Lua::Library::inst().close(L);
+}
+
+BOOST_AUTO_TEST_CASE(testPushValue)
+{
+	Lua::Library::State* L = Lua::Library::inst().newstate();
+
+	Lua::Library::inst().pushinteger(L, 10);
+	Lua::Library::inst().pushvalue(L, 1);
+
+	BOOST_TEST(Lua::Library::inst().gettop(L) == 2);
+
+	BOOST_TEST(Lua::Library::inst().tointeger(L, 1) == 10);
+	BOOST_TEST(Lua::Library::inst().tointeger(L, 2) == 10);
+
+	Lua::Library::inst().close(L);
+}
+
+BOOST_AUTO_TEST_CASE(testRemove)
+{
+	Lua::Library::State* L = Lua::Library::inst().newstate();
+
+	Lua::Library::inst().pushinteger(L, 10);
+	Lua::Library::inst().pushinteger(L, 20);
+	Lua::Library::inst().pushinteger(L, 30);
+
+	BOOST_TEST(Lua::Library::inst().gettop(L) == 3);
+
+	Lua::Library::inst().remove(L, 2);
+
+	BOOST_TEST(Lua::Library::inst().gettop(L) == 2);
+
+	BOOST_TEST(Lua::Library::inst().tointeger(L, 1) == 10);
+	BOOST_TEST(Lua::Library::inst().tointeger(L, 2) == 30);
+
+	Lua::Library::inst().close(L);
+}
+
+BOOST_AUTO_TEST_CASE(testInsert)
+{
+	Lua::Library::State* L = Lua::Library::inst().newstate();
+
+	Lua::Library::inst().pushinteger(L, 10);
+	Lua::Library::inst().pushinteger(L, 30);
+	Lua::Library::inst().pushinteger(L, 20);
+
+	BOOST_TEST(Lua::Library::inst().gettop(L) == 3);
+
+	Lua::Library::inst().insert(L, 2);
+
+	BOOST_TEST(Lua::Library::inst().gettop(L) == 3);
+
+	BOOST_TEST(Lua::Library::inst().tointeger(L, 1) == 10);
+	BOOST_TEST(Lua::Library::inst().tointeger(L, 2) == 20);
+	BOOST_TEST(Lua::Library::inst().tointeger(L, 3) == 30);
+
+	Lua::Library::inst().close(L);
+}
+
+BOOST_AUTO_TEST_CASE(testReplace)
+{
+	Lua::Library::State* L = Lua::Library::inst().newstate();
+
+	Lua::Library::inst().pushinteger(L, 10);
+	Lua::Library::inst().pushinteger(L, 20);
+	Lua::Library::inst().pushinteger(L, 30);
+	Lua::Library::inst().pushinteger(L, 40);
+
+	BOOST_TEST(Lua::Library::inst().gettop(L) == 4);
+
+	Lua::Library::inst().replace(L, 2);
+
+	BOOST_TEST(Lua::Library::inst().gettop(L) == 3);
+
+	BOOST_TEST(Lua::Library::inst().tointeger(L, 1) == 10);
+	BOOST_TEST(Lua::Library::inst().tointeger(L, 2) == 40);
+	BOOST_TEST(Lua::Library::inst().tointeger(L, 3) == 30);
+
+	Lua::Library::inst().close(L);
+}
+BOOST_AUTO_TEST_CASE(testBoolean)
+{
+	Lua::Library::State* L = Lua::Library::inst().newstate();
+
+	Lua::Library::inst().pushboolean(L, 0);
+	Lua::Library::inst().pushboolean(L, 1);
+
+	BOOST_TEST(Lua::Library::inst().isboolean(L, -1));
+	BOOST_TEST(Lua::Library::inst().isboolean(L, -2));
+
+	BOOST_TEST(Lua::Library::inst().toboolean(L, -1));
+	BOOST_TEST(!Lua::Library::inst().toboolean(L, -2));
+
+	Lua::Library::inst().close(L);
+}
+
+BOOST_AUTO_TEST_CASE(testNil)
+{
+	Lua::Library::State* L = Lua::Library::inst().newstate();
+
+	Lua::Library::inst().pushnil(L);
+	BOOST_TEST(Lua::Library::inst().gettop(L) == 1);
+	BOOST_TEST(Lua::Library::inst().isnil(L, -1));
+
+	Lua::Library::inst().close(L);
+}
+
+BOOST_AUTO_TEST_CASE(testNumber)
+{
+	Lua::Library::State* L = Lua::Library::inst().newstate();
+
+	Lua::Library::inst().pushnumber(L, 123.5);
+
+	BOOST_TEST(Lua::Library::inst().isnumber(L, -1));
+	BOOST_TEST(Lua::Library::inst().tonumber(L, -1) == 123.5);
+
+	Lua::Library::inst().close(L);
+}
+
+BOOST_AUTO_TEST_CASE(testInteger)
+{
+	Lua::Library::State* L = Lua::Library::inst().newstate();
+
+	Lua::Library::inst().pushinteger(L, 12345);
+
+	BOOST_TEST(Lua::Library::inst().isnumber(L, -1));
+	BOOST_TEST(Lua::Library::inst().tointeger(L, -1) == 12345);
+
+	Lua::Library::inst().close(L);
+}
+
+BOOST_AUTO_TEST_CASE(testString)
+{
+	Lua::Library::State* L = Lua::Library::inst().newstate();
+
+	Lua::Library::inst().pushstring(L, "Hi!");
+	Lua::Library::inst().pushlstring(L, "ABCDEF", 3);
+
+	BOOST_TEST(Lua::Library::inst().isstring(L, -1));
+	BOOST_TEST(Lua::Library::inst().isstring(L, -2));
+
+	BOOST_TEST(Lua::Library::inst().tostring(L, -2) == "Hi!");
+
+	size_t len = 0;
+	BOOST_TEST(Lua::Library::inst().tolstring(L, -1, &len) == "ABC");
+	BOOST_TEST(len == 3);
+
+	Lua::Library::inst().close(L);
+}
+
+BOOST_AUTO_TEST_CASE(testTable)
+{
+	Lua::Library::State* L = Lua::Library::inst().newstate();
+
+	Lua::Library::inst().createtable(L, 0, 0);
+
+	// t['num'] = 123
+	Lua::Library::inst().pushstring(L, "num");
+	Lua::Library::inst().pushinteger(L, 123);
+	Lua::Library::inst().settable(L, -3);
+
+	BOOST_TEST(Lua::Library::inst().gettop(L) == 1);
+	BOOST_TEST(Lua::Library::inst().istable(L, 1));
+
+	// t['str'] = "Hi!"
+	Lua::Library::inst().pushstring(L, "Hi!");
+	Lua::Library::inst().setfield(L, -2, "str");
+
+	BOOST_TEST(Lua::Library::inst().gettop(L) == 1);
+	BOOST_TEST(Lua::Library::inst().istable(L, 1));
+
+	// get field 'num'
+	Lua::Library::inst().pushstring(L, "num");
+	Lua::Library::inst().gettable(L, -2);
+
+	BOOST_TEST(Lua::Library::inst().gettop(L) == 2);
+	BOOST_TEST(Lua::Library::inst().tointeger(L, -1) == 123);
+	Lua::Library::inst().pop(L, 1);
+
+	// get field 'str'
+	Lua::Library::inst().getfield(L, 1, "str");
+
+	BOOST_TEST(Lua::Library::inst().gettop(L) == 2);
+	BOOST_TEST(Lua::Library::inst().tostring(L, -1) == "Hi!");
+	Lua::Library::inst().pop(L, 1);
+
+	Lua::Library::inst().close(L);
+}
+
+BOOST_AUTO_TEST_CASE(testMetatable)
+{
+	Lua::Library::State* L = Lua::Library::inst().newstate();
+
+	Lua::Library::inst().createtable(L, 0, 0);
+
+	BOOST_TEST(!Lua::Library::inst().getmetatable(L, 1));
+	BOOST_TEST(Lua::Library::inst().gettop(L) == 1);
+
+	Lua::Library::inst().createtable(L, 0, 0);
+	Lua::Library::inst().setmetatable(L, 1);
+	BOOST_TEST(Lua::Library::inst().gettop(L) == 1);
+
+	BOOST_TEST(Lua::Library::inst().getmetatable(L, 1));
+	BOOST_TEST(Lua::Library::inst().gettop(L) == 2);
+
+	Lua::Library::inst().close(L);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
