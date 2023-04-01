2f49334f52a5c49019f8f9ea98e3b9eb03e8a069
Petr Suchý <petrs.sokolo@seznam.cz>
2023-04-01 23:12:26 +0200

    Extended test cases for function calls.

--- tests/src/Engine/FunctionCallTests.cpp
+++ tests/src/Engine/FunctionCallTests.cpp
@@ -5,6 +5,101 @@
 
 BOOST_AUTO_TEST_SUITE(Engine__FunctionCall)
 
+BOOST_AUTO_TEST_CASE(testUnreadInputArguments)
+{
+	Lua::Engine lua;
+
+	lua.global("foo").in() << Lua::MakeFunc(
+		[](Lua::Args args, Lua::Lua lua)
+		{
+			if (args.in().count() != 0) {
+				throw std::logic_error(
+					"unexpected input arguments"
+				);
+			}
+		}
+	);
+
+	lua.global("bar").in() << Lua::MakeFunc(
+		[](Lua::Args args, Lua::Lua lua)
+		{
+			if (args.in().count() != 2) {
+				throw std::logic_error(
+					"unexpected number of input arguments"
+				);
+			}
+
+			lua.pcall("foo");
+
+			if (args.out().count() != 0) {
+				throw std::logic_error(
+					"unexpected output arguments"
+				);
+			}
+		}
+	);
+
+	lua.pcall("bar", 1, 2);
+}
+
+BOOST_AUTO_TEST_CASE(testUnreadInputArguments2)
+{
+	Lua::Engine lua;
+
+	lua.global("foo").in() << Lua::MakeFunc(
+		[](Lua::Args args, Lua::Lua lua)
+		{
+			if (args.in().count() != 2) {
+				throw std::logic_error(
+					"unexpected number of input arguments"
+				);
+			}
+
+			int n1;
+			args.in() >> n1;
+
+			if (n1 != 3) {
+				throw std::logic_error(
+					"unexpected value of the first input argument (" + std::to_string(n1) + ")"
+				);
+			}
+
+			int n2;
+			args.in() >> n2;
+
+			if (n2 != 4) {
+				throw std::logic_error(
+					"unexpected value of the second input argument (" + std::to_string(n2) + ")"
+				);
+			}
+		}
+	);
+
+	lua.global("bar").in() << Lua::MakeFunc(
+		[](Lua::Args args, Lua::Lua lua)
+		{
+			if (args.in().count() != 2) {
+				throw std::logic_error(
+					"unexpected number of input arguments"
+				);
+			}
+
+			int n1;
+			args.in() >> n1;
+
+			lua.pcall("foo", 3, 4);
+
+			if (args.out().count() != 0) {
+				throw std::logic_error(
+					"unexpected output arguments"
+				);
+			}
+		}
+	);
+
+	lua.pcall("bar", 1, 2);
+}
+
 BOOST_AUTO_TEST_CASE(testUnreadOutputArguments)
 {
 	Lua::Engine lua;
@@ -58,7 +153,7 @@ BOOST_AUTO_TEST_CASE(testUnreadOutputArguments2)
 
 			if (n1 != 3) {
 				throw std::logic_error(
-					"unexpected value of the first input argument"
+					"unexpected value of the first input argument (" + std::to_string(n1) + ")"
 				);
 			}
 
@@ -67,7 +162,7 @@ BOOST_AUTO_TEST_CASE(testUnreadOutputArguments2)
 
 			if (n2 != 4) {
 				throw std::logic_error(
-					"unexpected value of the second input argument"
+					"unexpected value of the second input argument (" + std::to_string(n2) + ")"
 				);
 			}
 		}
@@ -83,6 +178,58 @@ BOOST_AUTO_TEST_CASE(testUnreadOutputArguments2)
 
 	lua.pcall("bar", 3, 4);
 	BOOST_TEST(lua.args().out().count() == 0);
+}
+
+BOOST_AUTO_TEST_CASE(testUnreadOutputArguments3)
+{
+	Lua::Engine lua;
+
+	lua.global("foo").in() << Lua::MakeFunc(
+		[](Lua::Args args, Lua::Lua lua)
+		{
+			args.out() << 1 << 2;
+		}
+	);
+
+	lua.global("bar").in() << Lua::MakeFunc(
+		[](Lua::Args args, Lua::Lua lua)
+		{
+			if (args.in().count() != 2) {
+				throw std::logic_error(
+					"unexpected number of input arguments"
+				);
+			}
+
+			int n1;
+			args.in() >> n1;
+
+			if (n1 != 3) {
+				throw std::logic_error(
+					"unexpected value of the first input argument (" + std::to_string(n1) + ")"
+				);
+			}
+
+			int n2;
+			args.in() >> n2;
+
+			if (n2 != 4) {
+				throw std::logic_error(
+					"unexpected value of the second input argument (" + std::to_string(n2) + ")"
+				);
+			}
+		}
+	);
+
+	lua.global("cow").in() << Lua::MakeFunc(
+		[](Lua::Args args, Lua::Lua lua)
+		{
+			int n1;
+			lua.pcall("foo") >> n1;
+			lua.pcall("bar", 3, 4);
+		}
+	);
+
+	lua.pcall("cow");
 }
 
 BOOST_AUTO_TEST_CASE(testArgumentsOrder)
@@ -169,53 +316,100 @@ BOOST_AUTO_TEST_CASE(testArgumentsOrder)
 
 BOOST_AUTO_TEST_CASE(testForwardArguments)
 {
-	Lua::Engine lua(
-		[](Lua::Lua lua)
-		{
-			lua.global("forward").in() << Lua::MakeFunc(
-				[](Lua::Args args, Lua::Lua lua)
-				{
-					Lua::Function f;
-					args.in() >> f;
+	Lua::Engine lua;
 
-					while (!args.in().atEnd()) {
-						lua.args().in() << args.in();
-					}
-
-					lua.pcall(f);
-
-					while (!lua.args().out().atEnd()) {
-						lua.args().out() >> args.out();
-					}
-				}
-			);
-		}
-	);
-
-	auto implode = Lua::MakeFunc(
+	lua.global("foo").in() << Lua::MakeFunc(
 		[](Lua::Args args, Lua::Lua lua)
 		{
-			std::stringstream ss;
-			int n;
+			int result = 0;
+			int i = 1;
 
 			while (!args.in().atEnd()) {
-
+				int n;
 				args.in() >> n;
-				ss << n;
-
-				if (!args.in().atEnd()) {
-					ss << ", ";
-				}
+				result += n * i;
+				i *= 10;
 			}
 
-			args.out() << ss.str();
+			args.out() << result;
+
+			do {
+				args.out() << result % 10;
+				result /= 10;
+			} while (result != 0);
 		}
 	);
 
-	std::string result;
-	lua.pcall("forward", implode, 10, 20, 30, 40) >> result;
+	lua.global("bar").in() << Lua::MakeFunc(
+		[](Lua::Args args, Lua::Lua lua)
+		{
+			while (!args.in().atEnd()) {
+				lua.args().in() << args.in();
+			}
 
-	BOOST_TEST(result == "10, 20, 30, 40");
+			lua.pcall("foo");
+
+			while (!lua.args().out().atEnd()) {
+				lua.args().out() >> args.out();
+			}
+		}
+	);
+
+	int result;
+	int n1, n2, n3, n4, n5, n6;
+
+	lua.args().in() << 1;
+	lua.pcall("bar");
+	lua.args().out() >> result >> n1;
+	BOOST_TEST(result == 1);
+	BOOST_TEST(n1 == 1);
+	
+	lua.args().in() << 1;
+	lua.args().in() << 2;
+	lua.pcall("bar") >> result >> n1 >> n2;
+	BOOST_TEST(result == 21);
+	BOOST_TEST(n1 == 1);
+	BOOST_TEST(n2 == 2);
+	
+	lua.args().in() << 1;
+	lua.args().in() << 2;
+	lua.args().in() << 3;
+	lua.pcall("bar") >> result >> n1 >> n2 >> n3;
+	BOOST_TEST(result == 321);
+	BOOST_TEST(n1 == 1);
+	BOOST_TEST(n2 == 2);
+	BOOST_TEST(n3 == 3);
+
+	lua.args().in() << 1;
+	lua.args().in() << 2;
+	lua.args().in() << 3;
+	lua.pcall("bar", 4) >> result >> n1 >> n2 >> n3 >> n4;
+	BOOST_TEST(result == 4321);
+	BOOST_TEST(n1 == 1);
+	BOOST_TEST(n2 == 2);
+	BOOST_TEST(n3 == 3);
+	BOOST_TEST(n4 == 4);
+
+	lua.args().in() << 1;
+	lua.args().in() << 2;
+	lua.args().in() << 3;
+	lua.pcall("bar", 4, 5) >> result >> n1 >> n2 >> n3 >> n4 >> n5;
+	BOOST_TEST(result == 54321);
+	BOOST_TEST(n1 == 1);
+	BOOST_TEST(n2 == 2);
+	BOOST_TEST(n3 == 3);
+	BOOST_TEST(n4 == 4);
+	BOOST_TEST(n5 == 5);
+
+	lua.pcall("bar", 1, 2, 3, 4, 5, 6) >> result >> n1 >> n2 >> n3 >> n4 >> n5 >> n6;
+	BOOST_TEST(result == 654321);
+	BOOST_TEST(n1 == 1);
+	BOOST_TEST(n2 == 2);
+	BOOST_TEST(n3 == 3);
+	BOOST_TEST(n4 == 4);
+	BOOST_TEST(n5 == 5);
+	BOOST_TEST(n6 == 6);
+	
 }
 
 BOOST_AUTO_TEST_SUITE_END()
